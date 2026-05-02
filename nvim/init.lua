a84df412ef2a63928ecdbf2ada8b2f50d917ad21
samyepik <samuel.steinhart@gmail.com>
2026-05-02 12:28:01 +0200

    config updates to updated programs and gesture changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.filetype.add({
+  extension = { uss = "css" },
+})
+
+vim.filetype.add({
+  extension = {
+    uxml = "xml",
+  },
+})
---
+++ nvim/lua/plugins/arduino.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        arduino_language_server = {
+          cmd = {
+            "arduino-language-server",
+            "-cli-config",
+            vim.fn.expand("~/.arduino15/arduino-cli.yaml"),
+          },
+        },
+      },
+    },
+  },
+}
